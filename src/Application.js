d8851aae16fef053733c39a850aa557d71b7b321
chris-kyle2 <22ms0010@iitism.ac.in>
2024-10-18 15:29:13 +0530

    removed underscores

--- src/Application.js
+++ src/Application.js
@@ -13,97 +13,97 @@ function Application() {
           <NavigationLink to="/">Home</NavigationLink>
           {/* Updated Links for Each Linux Lesson */}
           <NavigationLink to="/notes/The_Call_to_Security: Fortifying OpenSSH Against Intruders">
-            The_Call_to_Security: Fortifying OpenSSH Against Intruders
+            The Call to Security: Fortifying OpenSSH Against Intruders
           </NavigationLink>
           <NavigationLink to="/notes/Guarding_the_Gates: Securing the GRUB Bootloader">
-            Guarding_the_Gates: Securing the GRUB Bootloader
+            Guarding the Gates: Securing the GRUB Bootloader
           </NavigationLink>
           <NavigationLink to="/notes/Dharma_of_Defense: Enforcing Strong Password Policies">
-            Dharma_of_Defense: Enforcing Strong Password Policies
+            Dharma of Defense: Enforcing Strong Password Policies
           </NavigationLink>
           <NavigationLink to="/notes/The_Account_of_Karma: Managing User Accounts Effectively">
-            The_Account_of_Karma: Managing User Accounts Effectively
+            The Account of Karma: Managing User Accounts Effectively
           </NavigationLink>
           <NavigationLink to="/notes/Commanding_the_Power: Wisely Using Sudo Privileges">
-            Commanding_the_Power: Wisely Using Sudo Privileges
+            Commanding the Power: Wisely Using Sudo Privileges
           </NavigationLink>
           <NavigationLink to="/notes/The_Chaos_of_the_Fork: Mitigating Fork Bombs">
-            The_Chaos_of_the_Fork: Mitigating Fork Bombs
+            The Chaos of the Fork: Mitigating Fork Bombs
           </NavigationLink>
           <NavigationLink to="/notes/The_Test_of_Strength: Auditing Password Resilience">
-            The_Test_of_Strength: Auditing Password Resilience
+            The Test of Strength: Auditing Password Resilience
           </NavigationLink>
           <NavigationLink to="/notes/The_Siege_of_the_Fortress: Strengthening Password Security">
-            The_Siege_of_the_Fortress: Strengthening Password Security
+            The Siege of the Fortress: Strengthening Password Security
           </NavigationLink>
           <NavigationLink to="/notes/The_Vigil_of_Integrity: Setting Up AIDE for Monitoring">
-            The_Vigil_of_Integrity: Setting Up AIDE for Monitoring
+            The Vigil of Integrity: Setting Up AIDE for Monitoring
           </NavigationLink>
           <NavigationLink to="/notes/The_Hidden_Enemy: Detecting Rootkits">
-            The_Hidden_Enemy: Detecting Rootkits
+            The Hidden Enemy: Detecting Rootkits
           </NavigationLink>
           <NavigationLink to="/notes/The_Sword_of_Purity: Utilizing ClamAV for Malware Protection">
-            The_Sword_of_Purity: Utilizing ClamAV for Malware Protection
+            The Sword of Purity: Utilizing ClamAV for Malware Protection
           </NavigationLink>
           <NavigationLink to="/notes/The_Shield_of_Encryption: Implementing Disk Encryption">
-            The_Shield_of_Encryption: Implementing Disk Encryption
+            The Shield of Encryption: Implementing Disk Encryption
           </NavigationLink>
           <NavigationLink to="/notes/The_Key_to_the_Hidden_Realm: Managing Encryption Keys">
-            The_Key_to_the_Hidden_Realm: Managing Encryption Keys
+            The Key to the Hidden Realm: Managing Encryption Keys
           </NavigationLink>
           <NavigationLink to="/notes/The_Scroll_of_Secrets: Using GPG for File Encryption">
-            The_Scroll_of_Secrets: Using GPG for File Encryption
+            The Scroll of Secrets: Using GPG for File Encryption
           </NavigationLink>
           <NavigationLink to="/notes/The_Guarded_Word: Securing Text with EncryptPad">
-            The_Guarded_Word: Securing Text with EncryptPad
+            The Guarded Word: Securing Text with EncryptPad
           </NavigationLink>
           <NavigationLink to="/notes/The_Art_of_Concealment: Mastering Steganography Techniques">
-            The_Art_of_Concealment: Mastering Steganography Techniques
+            The Art of Concealment: Mastering Steganography Techniques
           </NavigationLink>
           <NavigationLink to="/notes/The_Veil_of_Secrecy: Exploring Steghide for Hidden Data">
-            The_Veil_of_Secrecy: Exploring Steghide for Hidden Data
+            The Veil of Secrecy: Exploring Steghide for Hidden Data
           </NavigationLink>
           <NavigationLink to="/notes/The_All_Seeing_Eye: Scanning Networks with Nmap">
-            The_All_Seeing_Eye: Scanning Networks with Nmap
+            The All Seeing Eye: Scanning Networks with Nmap
           </NavigationLink>
           <NavigationLink to="/notes/Beyond_the_Horizon: Advanced Features of Nmap">
-            Beyond_the_Horizon: Advanced Features of Nmap
+            Beyond the Horizon: Advanced Features of Nmap
           </NavigationLink>
           <NavigationLink to="/notes/The_Barrier_of_Defense: Understanding iptables and Netfilter">
-            The_Barrier_of_Defense: Understanding iptables and Netfilter
+            The Barrier of Defense: Understanding iptables and Netfilter
           </NavigationLink>
           <NavigationLink to="/notes/The_Command_of_Protection: Wielding iptables Commands">
-            The_Command_of_Protection: Wielding iptables Commands
+            The Command of Protection: Wielding iptables Commands
           </NavigationLink>
           <NavigationLink to="/notes/The_Craft_of_the_Warrior: Writing Effective iptables Rules">
-            The_Craft_of_the_Warrior: Writing Effective iptables Rules
+            The Craft of the Warrior: Writing Effective iptables Rules
           </NavigationLink>
           <NavigationLink to="/notes/The_Path_of_Control: Defining iptables Policies">
-            The_Path_of_Control: Defining iptables Policies
+            The Path of Control: Defining iptables Policies
           </NavigationLink>
           <NavigationLink to="/notes/The_Cleansing_Fire: Flushing iptables for Fresh Configurations">
-            The_Cleansing_Fire: Flushing iptables for Fresh Configurations
+            The Cleansing Fire: Flushing iptables for Fresh Configurations
           </NavigationLink>
           <NavigationLink to="/notes/The_Art_of_Precision: Filtering Traffic by IP Addresses">
-            The_Art_of_Precision: Filtering Traffic by IP Addresses
+            The Art of Precision: Filtering Traffic by IP Addresses
           </NavigationLink>
           <NavigationLink to="/notes/The_Ports_of_Battle: Securing Digital Entry Points">
-            The_Ports_of_Battle: Securing Digital Entry Points
+            The Ports of Battle: Securing Digital Entry Points
           </NavigationLink>
           <NavigationLink to="/notes/The_Heart_of_the_Shield: The Importance of Stateful Firewalls">
-            The_Heart_of_the_Shield: The Importance of Stateful Firewalls
+            The Heart of the Shield: The Importance of Stateful Firewalls
           </NavigationLink>
           <NavigationLink to="/notes/The_Defensive_Wall: Setting Up Stateful Firewall Rules">
-            The_Defensive_Wall: Setting Up Stateful Firewall Rules
+            The Defensive Wall: Setting Up Stateful Firewall Rules
           </NavigationLink>
           <NavigationLink to="/notes/The_Mark_of_Identity: Matching Packets by MAC Address">
-            The_Mark_of_Identity: Matching Packets by MAC Address
+            The Mark of Identity: Matching Packets by MAC Address
           </NavigationLink>
           <NavigationLink to="/notes/The_Keeper_of_Time: Controlling Access by Date and Time">
-            The_Keeper_of_Time: Controlling Access by Date and Time
+            The Keeper of Time: Controlling Access by Date and Time
           </NavigationLink>
           <NavigationLink to="/notes/The_Path_of_Action: Understanding ACCEPT and DROP Targets in iptables">
-            The_Path_of_Action: Understanding ACCEPT and DROP Targets in iptables
+            The Path of Action: Understanding ACCEPT and DROP Targets in iptables
           </NavigationLink>
         </Navigation>
 
